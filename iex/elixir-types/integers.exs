22e85de7737f26f2c2618f3b0920d70c47c49008
Exadra37 <exadra37@gmail.com>
2017-07-06 09:04:14 +0100

    Chapter 4 > Elixir Basics > Value Types > Integers

---
+++ iex/elixir-types/integers.exs
@@ -0,0 +1,16 @@
+# Chapter 4 > Elixir Basics > Value Types > Integers
+
+IO.puts "
+
+In Elixir there is no fixed limit for the size of integers.
+
+Integer literals can be represented as:
+	
+	* decimal - 1234
+	* hexadecimal - 0xcafe
+	* octal - 0o765
+	* binary - 0b1010
+
+On decimal integers we can use underscores to separate groups of digits, like 1_000_000
+
+"
