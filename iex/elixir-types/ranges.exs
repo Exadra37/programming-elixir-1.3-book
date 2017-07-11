b6fdbf79dc0446d917ca2996e76d5d61c1a1e51f
Exadra37 <exadra37@gmail.com>
2017-07-11 19:46:31 +0100

    Chapter 4 > Elixir Basics > Value Types > Ranges

---
+++ iex/elixir-types/ranges.exs
@@ -0,0 +1,23 @@
+# Chapter 4 > Elixir Basics > Value Types > Ranges
+
+IO.puts "
+Ranges will have a start and end represented by integers separated by 2 dots.
+"
+
+IO.puts "
+iex> 1..3"
+IO.inspect(1..3)
+
+IO.puts "
+iex> i 1..3
+Term
+  1..3
+Data type
+  Range
+Description
+  This is a struct. Structs are maps with a __struct__ key.
+Reference modules
+  Range, Map
+Implemented protocols
+  IEx.Info, Enumerable, Inspect
+"
