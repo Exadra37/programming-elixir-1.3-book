26250ba42a54dc896dd0cc0e8897450e0851cc1c
Exadra37 <exadra37@gmail.com>
2017-07-15 15:42:59 +0100

    Chapter 4 > Elixir Basics > System Types > References

---
+++ iex/elixir-types/references.exs
@@ -0,0 +1,10 @@
+# Chapter 4 > Elixir Basics > System Types > References
+
+IO.puts "
+
+Using the function make_ref we will create globally a unique reference.
+"
+
+IO.puts "
+iex> make_ref()"
+IO.inspect(make_ref())
