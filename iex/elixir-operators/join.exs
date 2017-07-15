ae589046d6dd005ae9b833d854ff4b1aa7a6956b
Exadra37 <exadra37@gmail.com>
2017-07-15 21:36:54 +0100

    Chapter 4 > Elixir Basics > Operators > Join

---
+++ iex/elixir-operators/join.exs
@@ -0,0 +1,20 @@
+# Chapter 4 > Elixir Operators > Operators > Join
+
+IO.puts "
+Join Operators
+	→ <> # concatenates binaries or strings
+	→ ++ # concatenates 2 lists
+	→ -- # removes elements from a list by returning another list without them
+"
+
+IO.puts "
+iex> \"Hello\" <> \" World\""
+IO.inspect("Hello" <> " World")
+
+IO.puts "
+iex> [\"Hello\", \"World\"] ++ [\"Learning\", \"Elixir\"]"
+IO.inspect(["Hello", "World"] ++ ["Learning", "Elixir"])
+
+IO.puts "
+iex> [\"Hello\", \"World\"] -- [\"World\"]"
+IO.inspect(["Hello", "World"] -- ["World"])
