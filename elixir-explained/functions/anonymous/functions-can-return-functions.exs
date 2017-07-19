06cc049f38aef46cae1510abfe7c508f06abf0af
Exadra37 <exadra37@gmail.com>
2017-07-19 18:25:28 +0100

    Chapter 5 > Anonymous Functions > Functions Can Return Functions

--- elixir-explained/functions/anonymous/functions-can-return-functions.exs
+++ elixir-explained/functions/anonymous/functions-can-return-functions.exs
@@ -6,12 +6,29 @@ Functions Can Return Functions
 
 Really...
 
+Signature withour parameters:
+
 fn -> (fn -> body end) end
 
 fn ->
     fn ->
         body
     end
+end
+
+
+Signature with parameters:
+
+fn (outer_parameters)-> (fn (inner_parameters) -> body end) end
+
+fn (outer_parameters) ->
+
+    fn (inner_parameters)->
+
+        # outer parameters are available inside the inner function
+        body
+    end
+
 end
 "
 
@@ -20,7 +37,7 @@ outer_function = fn -> (fn -> "Hello from Inner Function" end) end
 inner_function = outer_function.()
 
 IO.puts "
-Let's give it a try:
+---> Example Without Parameters <---
 
 outer_function = fn -> (fn -> \"Hello from Inner Function\" end) end
 
@@ -36,8 +53,37 @@ inner_function = outer_function.()
 IO.inspect(inner_function)"
 IO.inspect(inner_function)
 
-
 IO.puts "
 # this time we can invoke directly the inner function without needing to use the outer function
 IO.puts(inner_function.())"
 IO.puts(inner_function.())
+
+
+
+IO.puts "
+
+---> Example With Parameters <---
+
+value = fn (number) ->
+            fn (increment) ->
+                # variable number is accessible here because inner functions always carry with them the parameters of
+                # the outer function.
+                number + increment
+            end
+        end
+
+# Getting the inner function and setting is value to 2
+increment_by = value.(2)
+
+# Using the inner function to increment the value 2 of the outer function by 4
+IO.puts(increment_by.(4))"
+
+value = fn (number) ->
+            fn (increment) ->
+                number + increment
+            end
+        end
+
+increment_by = value.(2)
+
+IO.puts(increment_by.(4))
