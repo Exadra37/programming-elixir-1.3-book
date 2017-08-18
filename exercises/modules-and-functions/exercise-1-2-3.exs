e0d5033edeaa9a00f4c4d98e57e718ac41e1a586
Exadra37 <exadra37@gmail.com>
2017-08-18 07:35:30 +0000

    Chapter 6 > Modules and Named Functions > Exercise 1, 2 and 3.

---
+++ exercises/modules-and-functions/exercise-1-2-3.exs
@@ -0,0 +1,18 @@
+# Chapter 6 > Modules and Named Functions > Exercise 1, 2 and 3
+# From page 49
+
+defmodule Times do
+
+  def double(number) do
+    number * 2
+  end
+
+  def triple(number) do
+    number * 3
+  end
+
+  def quadruple(number) do
+    double(number) + double(number)
+  end
+
+end
