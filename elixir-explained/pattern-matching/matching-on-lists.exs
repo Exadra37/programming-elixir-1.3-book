a8df4e415a26d306cc530250b7f4e9593541d46e
exadra37 <exadra37@gmail.com>
2017-12-18 08:51:08 +0000

    Update typo on matching-on-lists.exs

--- elixir-explained/pattern-matching/matching-on-lists.exs
+++ elixir-explained/pattern-matching/matching-on-lists.exs
@@ -38,7 +38,7 @@ IO.puts "\nSo Elixir is a magic potion, do you remember eh...\n"
 
 IO.puts "Another Example...\n"
 
-IO.puts "iex()> [var1, 1, var2] = list"
+IO.puts "iex()> [var1, 2, var2] = list"
 IO.inspect([var1, 2, var2] = list)
 IO.puts "# It is able to succeed because left and right hand side can be made with same values and structure:"
 IO.puts "# [1, 2, 3] = [1, 2, 3]\n"
